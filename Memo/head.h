7069cf2c1f21beda71424da5736b22d72e36b88d
Moon <128487492+Moondongoh@users.noreply.github.com>
2024-03-04 01:58:51 +0900

    inf

---
+++ Memo/head.h
@@ -0,0 +1,15 @@
+#pragma comment(lib, "imm32.lib")
+
+#define IDR_MENU1                       101
+#define ID_FILE_NEW                     40001
+#define ID_FILE_NEW_WINDOW              40002
+#define ID_FILE_OPEN                    40003
+#define ID_FILE_SAVE                    40004
+#define ID_FILE_RE_SAVE                 40005
+#define ID_FILE_EXIT                    40006
+#define ID_EDIT_UNDO                    40007
+#define ID_EDIT_CUT                     40008
+#define ID_EDIT_COPY                    40009
+#define ID_EDIT_PASTE                   40010
+#define ID_EDIT_DELETE                  40011
+#define ID_EDIT_SELECT_ALL              40015
