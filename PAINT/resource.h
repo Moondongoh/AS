30e766e724b47db7cba4afdf71e3c2f403a5df78
Moondongoh <mdo0421@naver.com>
2024-04-01 15:06:37 +0900

    ing

--- PAINT/resource.h
+++ PAINT/resource.h
@@ -37,7 +37,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40029
+#define _APS_NEXT_COMMAND_VALUE         40032
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
