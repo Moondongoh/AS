e3d46340f158866d4098578dbfa82eae0f1beb43
Moondongoh <mdo0421@naver.com>
2024-09-27 12:07:58 +0900

    [END]실시간__및_사진_객체_인식

--- YOLO_TEST/test.py
+++ YOLO_TEST/test.py
@@ -1,34 +1,62 @@
+# import cv2
+# from ultralytics import YOLO
+
+# # Load the YOLOv8 model
+# model = YOLO('yolov8n.pt')
+
+# # Open the webcam (use 0 for the default camera)
+# cap = cv2.VideoCapture(1)
+
+# # Loop through the webcam frames
+# while cap.isOpened():
+#     # Read a frame from the webcam
+#     success, frame = cap.read()
+
+#     if success:
+#         # Run YOLOv8 inference on the frame
+#         results = model(frame)
+
+#         # Visualize the results on the frame
+#         annotated_frame = results[0].plot()
+
+#         # Display the annotated frame
+#         cv2.imshow("YOLOv8 Webcam Inference", annotated_frame)
+
+#         # Break the loop if 'q' is pressed
+#         if cv2.waitKey(1) & 0xFF == ord("q"):
+#             break
+#     else:
+#         # Break the loop if there's an issue reading the frame
+#         break
+
+# # Release the webcam and close the display window
+# cap.release()
+# cv2.destroyAllWindows()
+
 import cv2
 from ultralytics import YOLO
 
 # Load the YOLOv8 model
 model = YOLO('yolov8n.pt')
 
-# Open the webcam (use 0 for the default camera)
-cap = cv2.VideoCapture(1)
+# Load the image (test.png)
+image = cv2.imread('D:/Git_Hub/AS/YOLO_TEST/t.jpg')
 
-# Loop through the webcam frames
-while cap.isOpened():
-    # Read a frame from the webcam
-    success, frame = cap.read()
+# Check if the image was loaded successfully
+if image is not None:
+    # Run YOLOv8 inference on the image
+    results = model(image)
 
-    if success:
-        # Run YOLOv8 inference on the frame
-        results = model(frame)
+    # Visualize the results on the image
+    annotated_image = results[0].plot()
 
-        # Visualize the results on the frame
-        annotated_frame = results[0].plot()
+    # Display the annotated image
+    cv2.imshow("YOLOv8 Image Inference", annotated_image)
 
-        # Display the annotated frame
-        cv2.imshow("YOLOv8 Webcam Inference", annotated_frame)
+    # Wait until a key is pressed to close the window
+    cv2.waitKey(0)
 
-        # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-    else:
-        # Break the loop if there's an issue reading the frame
-        break
-
-# Release the webcam and close the display window
-cap.release()
-cv2.destroyAllWindows()
+    # Close the display window
+    cv2.destroyAllWindows()
+else:
+    print("Error: Could not load image 'test.png'")
