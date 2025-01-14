937854b13f637c2336413b024d2fcf2aaf18857b
Moondongoh <mdo0421@naver.com>
2025-01-15 03:11:08 +0900

    모델_실행_메인인

---
+++ Car_Detection/YoLo/main.py
@@ -0,0 +1,10 @@
+from model_training.train_model import train_model
+from model_training.present_a import config as present
+from model_training.past_b import config as past
+
+if __name__ == "__main__":
+    print("Training on Dataset Present")
+    train_model(present)
+    
+    print("Training on Dataset Past")
+    train_model(past)
