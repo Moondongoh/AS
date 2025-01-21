dbb24d14f7bf1793bc00d8f3a0a6c1e17c2712ea
Moondongoh <mdo0421@naver.com>
2025-01-22 03:26:57 +0900

    Main

--- Car_Detection/YoLo/main.py
+++ Car_Detection/YoLo/main.py
@@ -8,3 +8,15 @@ if __name__ == "__main__":
     
     print("Training on Dataset Past")
     train_model(past)
+
+
+# from model_training.train_model_balance import train_model
+# from model_training.present_a import config as present
+# from model_training.past_b import config as past
+
+# if __name__ == "__main__":
+#     # print("Training on Dataset Present")
+#     # train_model(present)
+    
+#     print("Training on Dataset Past")
+#     train_model(past)
